572797ebe1df5c7f4ea05eb689a4a39d2c644c7f
math045b <62756914+math045b@users.noreply.github.com>
2020-03-31 00:16:47 +0200

    Added item model

--- src/main/java/com/cy4/alienexpansion/core/init/BlockInit.java
+++ src/main/java/com/cy4/alienexpansion/core/init/BlockInit.java
@@ -3,7 +3,7 @@ package com.cy4.alienexpansion.core.init;
 import com.cy4.alienexpansion.AlienExpansionMod;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,5 +12,5 @@ public class BlockInit {
 	
 	public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, AlienExpansionMod.MOD_ID);
 	
-	public static final RegistryObject<Block> SIMPLE_RUNE = BLOCKS.register("simple_rune", () -> new Block(Block.Properties.from(Blocks.STONE)));
+	public static final RegistryObject<Block> SIMPLE_RUNE = BLOCKS.register("simple_rune", () -> new Block(Block.Properties.create(Material.IRON)));
 }
