9d20cb5107308e51beb20fe027781366992d22d3
Cy4Shot <veschi@gmail.com>
2020-03-31 10:17:15 +0100

    cleanup

--- src/main/java/com/cy4/alienexpansion/core/world/dimension/AlienExpansionDimension.java
+++ src/main/java/com/cy4/alienexpansion/core/world/dimension/AlienExpansionDimension.java
@@ -20,7 +20,9 @@ public class AlienExpansionDimension extends Dimension {
 
 	@Override
 	public ChunkGenerator<?> createChunkGenerator() {
-		return new AlienExpansionChunkGenerator(world, BiomeProviderType.FIXED.create(BiomeProviderType.FIXED.func_226840_a_(this.world.getWorldInfo()).setBiome(BiomeInit.ALIEN_EXPANSION_VOID.get())), new AlienExpansionChunkGenSettings());
+		return new AlienExpansionChunkGenerator(world, BiomeProviderType.FIXED.create(BiomeProviderType.FIXED
+				.func_226840_a_(this.world.getWorldInfo()).setBiome(BiomeInit.ALIEN_EXPANSION_VOID.get())),
+				new AlienExpansionChunkGenSettings());
 	}
 
 	@Override
