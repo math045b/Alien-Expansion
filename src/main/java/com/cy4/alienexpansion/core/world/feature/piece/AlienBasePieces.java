2816c9c7b9cfe893a2c4f961b5c0fc413c38fadc
Cy4Shot <veschi@gmail.com>
2020-03-31 10:57:10 +0100

    More pieces

--- src/main/java/com/cy4/alienexpansion/core/world/feature/piece/AlienBasePieces.java
+++ src/main/java/com/cy4/alienexpansion/core/world/feature/piece/AlienBasePieces.java
@@ -15,4 +15,13 @@ public class AlienBasePieces {
 	public static final AlienBasePieceTemplate ARTIFACT_ROOM = new AlienBasePieceTemplate(
 			new boolean[] { true, true, true, true, false, false },
 			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.artifact_room"));
+	public static final AlienBasePieceTemplate CORRIDOR_NORTH_SOUTH = new AlienBasePieceTemplate(
+			new boolean[] { true, true, false, false, false, false },
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corridor.north_south"));
+	public static final AlienBasePieceTemplate CORRIDOR_EAST_WEST = new AlienBasePieceTemplate(
+			new boolean[] { false, false, true, true, false, false },
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corridor.east_west"));
+	public static final AlienBasePieceTemplate CORRIDOR_UP_DOWN = new AlienBasePieceTemplate(
+			new boolean[] { false, false, false, false, true, true },
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corridor.up_down"));
 }
