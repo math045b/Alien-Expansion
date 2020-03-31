691335f28c3de7413bc8e168f1bf8296a54bddd5
Cy4Shot <veschi@gmail.com>
2020-03-31 17:13:15 +0100

    Pillar Block Code

--- src/main/java/com/cy4/alienexpansion/core/init/BlockInit.java
+++ src/main/java/com/cy4/alienexpansion/core/init/BlockInit.java
@@ -3,6 +3,7 @@ package com.cy4.alienexpansion.core.init;
 import com.cy4.alienexpansion.AlienExpansionMod;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.RotatedPillarBlock;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,4 +17,6 @@ public class BlockInit {
 	public static final RegistryObject<Block> ADVANCED_RUNE = BLOCKS.register("advanced_rune", () -> new Block(PropertyInit.ALIEN_BLOCK));
 	public static final RegistryObject<Block> ALIEN_RUNE = BLOCKS.register("alien_rune", () -> new Block(PropertyInit.ALIEN_BLOCK));
 	public static final RegistryObject<Block> ALIEN_RUNE_ACTIVE = BLOCKS.register("alien_rune_active", () -> new Block(PropertyInit.LIT_ALIEN_BLOCK));
+	public static final RegistryObject<Block> ALIEN_PILLAR = BLOCKS.register("alien_pillar", () -> new RotatedPillarBlock(PropertyInit.ALIEN_BLOCK));
+	public static final RegistryObject<Block> ALIEN_PILLAR_ACTIVE = BLOCKS.register("alien_pillar_active", () -> new RotatedPillarBlock(PropertyInit.LIT_ALIEN_BLOCK));
 }
