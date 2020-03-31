c1e50e049c96579b3de72c15a2afa1b6c0cd9417
Cy4Shot <veschi@gmail.com>
2020-03-31 17:44:23 +0100

    Piece ID System

--- src/main/java/com/cy4/alienexpansion/core/world/feature/piece/AlienBasePieces.java
+++ src/main/java/com/cy4/alienexpansion/core/world/feature/piece/AlienBasePieces.java
@@ -5,43 +5,43 @@ import com.cy4.alienexpansion.AlienExpansionMod;
 import net.minecraft.util.ResourceLocation;
 
 public class AlienBasePieces {
-	public static final AlienBasePieceTemplate ARTIFACT_ROOM = new AlienBasePieceTemplate(0,
-			new boolean[] { true, true, true, true, false, false },
-			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.artifact_room"));
-
-	public static final AlienBasePieceTemplate CORRIDOR_NORTH_SOUTH = new AlienBasePieceTemplate(1,
+	public static final AlienBasePieceTemplate CORRIDOR_NORTH_SOUTH = new AlienBasePieceTemplate(0,
 			new boolean[] { true, true, false, false, false, false },
 			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corridor.north_south"));
 
-	public static final AlienBasePieceTemplate CORRIDOR_EAST_WEST = new AlienBasePieceTemplate(2,
+	public static final AlienBasePieceTemplate CORRIDOR_EAST_WEST = new AlienBasePieceTemplate(1,
 			new boolean[] { false, false, true, true, false, false },
 			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corridor.east_west"));
 
-	public static final AlienBasePieceTemplate CORRIDOR_UP_DOWN = new AlienBasePieceTemplate(3,
+	public static final AlienBasePieceTemplate CORRIDOR_UP_DOWN = new AlienBasePieceTemplate(2,
 			new boolean[] { false, false, false, false, true, true },
 			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corridor.up_down"));
 
-	public static final AlienBasePieceTemplate CORNER_NORTH = new AlienBasePieceTemplate(4,
+	public static final AlienBasePieceTemplate END_NORTH = new AlienBasePieceTemplate(3,
 			new boolean[] { true, false, false, false, false, false },
-			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corner.north"));
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.end.north"));
 
-	public static final AlienBasePieceTemplate CORNER_SOUTH = new AlienBasePieceTemplate(5,
+	public static final AlienBasePieceTemplate END_SOUTH = new AlienBasePieceTemplate(4,
 			new boolean[] { false, true, false, false, false, false },
-			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corner.south"));
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.end.south"));
 
-	public static final AlienBasePieceTemplate CORNER_EAST = new AlienBasePieceTemplate(6,
+	public static final AlienBasePieceTemplate END_EAST = new AlienBasePieceTemplate(5,
 			new boolean[] { false, false, true, false, false, false },
-			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corner.east"));
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.end.east"));
 
-	public static final AlienBasePieceTemplate CORNER_WEST = new AlienBasePieceTemplate(7,
+	public static final AlienBasePieceTemplate END_WEST = new AlienBasePieceTemplate(6,
 			new boolean[] { false, false, false, true, false, false },
-			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corner.west"));
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.end.west"));
 
-	public static final AlienBasePieceTemplate CORNER_UP = new AlienBasePieceTemplate(8,
+	public static final AlienBasePieceTemplate END_UP = new AlienBasePieceTemplate(7,
 			new boolean[] { false, false, false, false, true, false },
-			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corner.up"));
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.end.up"));
 
-	public static final AlienBasePieceTemplate CORNER_DOWN = new AlienBasePieceTemplate(9,
+	public static final AlienBasePieceTemplate END_DOWN = new AlienBasePieceTemplate(8,
 			new boolean[] { false, false, false, false, false, true },
-			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.corner.down"));
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.end.down"));
+	
+	public static final AlienBasePieceTemplate ARTIFACT_ROOM = new AlienBasePieceTemplate(9,
+			new boolean[] { true, true, true, true, false, false },
+			new ResourceLocation(AlienExpansionMod.MOD_ID, "alienbasepiece.room.artifact_room"));
 }
