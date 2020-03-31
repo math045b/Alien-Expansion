9a849d0eb522d15244d6e4ea9f4642b71391b87d
Cy4Shot <veschi@gmail.com>
2020-03-31 12:15:46 +0100

    cleanup

--- src/main/java/com/cy4/alienexpansion/common/items/ItemPortalTablet.java
+++ src/main/java/com/cy4/alienexpansion/common/items/ItemPortalTablet.java
@@ -12,23 +12,25 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 
-public class ItemPortalTablet extends Item
-{
+public class ItemPortalTablet extends Item {
 	public ItemPortalTablet(Item.Properties properties) {
 		super(properties);
 	}
-	
+
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) 
-	{
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
 		if (!worldIn.isRemote) {
 			AlienExpansionMod.LOGGER.debug("Portal Item triggeret! Start");
 			playerIn.changeDimension(DimensionType.byName(AlienExpansionMod.DIMENSION_TYPE));
 			for (int y = -3; y < 3; y++) {
 				for (int x = -3; x < 3; x++) {
 					for (int z = 0; z < 3; z++) {
-						playerIn.world.setBlockState(new BlockPos(playerIn.getPosition().getX() + x, playerIn.getPosition().getY() + y, playerIn.getPosition().getZ() + z), Blocks.AIR.getDefaultState());
-					}					
+						playerIn.world
+								.setBlockState(
+										new BlockPos(playerIn.getPosition().getX() + x,
+												playerIn.getPosition().getY() + y, playerIn.getPosition().getZ() + z),
+										Blocks.AIR.getDefaultState());
+					}
 				}
 			}
 			AlienExpansionMod.LOGGER.debug("Portal Item triggeret! End");
