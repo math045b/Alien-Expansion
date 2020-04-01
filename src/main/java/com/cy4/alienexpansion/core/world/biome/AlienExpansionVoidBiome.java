2209819591bb24c78534a69520ab536035ecf427
Cy4Shot <veschi@gmail.com>
2020-04-01 13:44:49 +0100

    Better Features!

--- src/main/java/com/cy4/alienexpansion/core/world/biome/AlienExpansionVoidBiome.java
+++ src/main/java/com/cy4/alienexpansion/core/world/biome/AlienExpansionVoidBiome.java
@@ -7,20 +7,19 @@ import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.placement.ChanceConfig;
-import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.DungeonRoom;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class AlienExpansionVoidBiome extends Biome {
-    public AlienExpansionVoidBiome(Builder props) {
-        super(props);
-        this.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES,
-        new AlienBaseFeature(NoFeatureConfig::deserialize)
-        .withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG)
-        .withPlacement(Placement.CHANCE_HEIGHTMAP_DOUBLE.configure(new ChanceConfig(1))));
-    }
-    
-    @OnlyIn(Dist.CLIENT)
+	public AlienExpansionVoidBiome(Builder props) {
+		super(props);
+		this.addFeature(GenerationStage.Decoration.RAW_GENERATION,
+				new AlienBaseFeature(NoFeatureConfig::deserialize).withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG)
+						.withPlacement(new DungeonRoom(ChanceConfig::deserialize).configure(new ChanceConfig(1))));
+	}
+
+	@OnlyIn(Dist.CLIENT)
 	public int getFoliageColor() {
 		return 203212166;
 	}
@@ -28,5 +27,5 @@ public class AlienExpansionVoidBiome extends Biome {
 	@OnlyIn(Dist.CLIENT)
 	public int getGrassColor(double posX, double posZ) {
 		return 9470285;
-	} 
+	}
 }
--- src/main/java/com/cy4/alienexpansion/core/world/feature/AlienBaseFeature.java
+++ src/main/java/com/cy4/alienexpansion/core/world/feature/AlienBaseFeature.java
@@ -16,7 +16,6 @@ import net.minecraft.world.gen.GenerationSettings;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.template.BlockIgnoreStructureProcessor;
-import net.minecraft.world.gen.feature.template.IntegrityProcessor;
 import net.minecraft.world.gen.feature.template.PlacementSettings;
 import net.minecraft.world.gen.feature.template.Template;
 import net.minecraft.world.server.ServerWorld;
@@ -33,20 +32,12 @@ public class AlienBaseFeature extends Feature<NoFeatureConfig> {
 		Rotation rotation = arotation[random.nextInt(arotation.length)];
 		Template template = ((ServerWorld) worldIn.getWorld()).getSaveHandler().getStructureTemplateManager()
 				.getTemplateDefaulted(AlienBasePieces.PIECES[random.nextInt(3)].structurefile);
-
-		PlacementSettings placementsettings = (new PlacementSettings()).setRotation(rotation)
-				// .setBoundingBox defines until where the structure should spawn, for example
-				// here it's max 25600 X, 256 Y and 25600 Z
-				.setBoundingBox(new MutableBoundingBox((0 - 25600), (0 - 25600), (0 - 25600), 25600, 256, 25600))
-				.setRandom(random).addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK);
-		// j k l are respectively x y z , and define the width, height and depth of the
-		// structure
-		BlockPos blockpos1 = template.getZeroPositionWithTransform(pos, Mirror.NONE, rotation);
-		IntegrityProcessor integrityprocessor = new IntegrityProcessor(0.9F);
-		placementsettings.clearProcessors().addProcessor(integrityprocessor);
-		template.addBlocksToWorld(worldIn, blockpos1, placementsettings, 4);
-		placementsettings.func_215220_b(integrityprocessor);
+		template.addBlocksToWorld(worldIn, template.getZeroPositionWithTransform(pos, Mirror.NONE, rotation),
+				(new PlacementSettings()).setRotation(rotation)
+						.setBoundingBox(
+								new MutableBoundingBox((0 - 25600), (0 - 25600), (0 - 25600), 25600, 256, 25600))
+						.setRandom(random).addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK),
+				4);
 		return true;
-
 	}
 }
