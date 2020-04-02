17e5d6b1280255d9947b4790696c9738c2a33eb4
Cy4Shot <veschi@gmail.com>
2020-04-02 11:23:54 +0100

    Creative Check and Crash Fix

--- src/main/java/com/cy4/alienexpansion/common/entity/item/DisplayCaseEntity.java
+++ src/main/java/com/cy4/alienexpansion/common/entity/item/DisplayCaseEntity.java
@@ -1,5 +1,6 @@
 package com.cy4.alienexpansion.common.entity.item;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.item.ItemFrameEntity;
 import net.minecraft.util.Direction;
@@ -7,12 +8,21 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class DisplayCaseEntity extends ItemFrameEntity {
-
-	public DisplayCaseEntity(EntityType<? extends ItemFrameEntity> p_i50224_1_, World p_i50224_2_) {
+	public DisplayCaseEntity(EntityType<? extends DisplayCaseEntity> p_i50224_1_, World p_i50224_2_) {
 		super(p_i50224_1_, p_i50224_2_);
 	}
-	
+
 	public DisplayCaseEntity(World worldIn, BlockPos p_i45852_2_, Direction p_i45852_3_) {
 		super(worldIn, p_i45852_2_, p_i45852_3_);
+	}
+
+	@Override
+	public void onBroken(Entity brokenEntity) {
+		for (int i = 0; i < this.world.getPlayers().size(); i++) {
+			if (!this.world.getPlayers().get(i).isCreative()) {
+				return;
+			}
+		}
+		super.onBroken(brokenEntity);
 	}
 }
