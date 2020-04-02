680b4dd20d7d8a0ad75847dae4ddba4b6571fd7c
Cy4Shot <veschi@gmail.com>
2020-04-02 10:23:52 +0100

    Changed Item Class

--- src/main/java/com/cy4/alienexpansion/core/init/ItemInit.java
+++ src/main/java/com/cy4/alienexpansion/core/init/ItemInit.java
@@ -14,5 +14,5 @@ public class ItemInit {
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, AlienExpansionMod.MOD_ID);	
 	public static final RegistryObject<Item> PORTAL_TABLET = ITEMS.register("portal_tablet", () -> new ItemPortalTablet(PropertyInit.BASIC_ITEM));	
 	public static final RegistryObject<Item> PEARL = ITEMS.register("pearl", () -> new Item(PropertyInit.BASIC_ITEM));
-	public static final RegistryObject<Item> DISPLAY_CASE = ITEMS.register("display_case", () -> new ItemDisplayCase(PropertyInit.BASIC_ITEM));	
+	public static final RegistryObject<Item> DISPLAY_CASE = ITEMS.register("display_case", () -> new ItemDisplayCase(PropertyInit.BASIC_ITEM));
 }
