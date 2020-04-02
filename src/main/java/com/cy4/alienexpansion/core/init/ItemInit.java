e49462c8dae5d7d75da0ac10c784d59a16e500cb
Cy4Shot <veschi@gmail.com>
2020-04-02 11:30:10 +0100

    Delete Display Case Item!

--- src/main/java/com/cy4/alienexpansion/core/init/ItemInit.java
+++ src/main/java/com/cy4/alienexpansion/core/init/ItemInit.java
@@ -1,7 +1,6 @@
 package com.cy4.alienexpansion.core.init;
 
 import com.cy4.alienexpansion.AlienExpansionMod;
-import com.cy4.alienexpansion.common.items.ItemDisplayCase;
 import com.cy4.alienexpansion.common.items.ItemPortalTablet;
 
 import net.minecraft.item.Item;
@@ -13,5 +12,4 @@ public class ItemInit {
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, AlienExpansionMod.MOD_ID);	
 	public static final RegistryObject<Item> PORTAL_TABLET = ITEMS.register("portal_tablet", () -> new ItemPortalTablet(PropertyInit.BASIC_ITEM));	
 	public static final RegistryObject<Item> PEARL = ITEMS.register("pearl", () -> new Item(PropertyInit.BASIC_ITEM));
-	public static final RegistryObject<Item> DISPLAY_CASE = ITEMS.register("display_case", () -> new ItemDisplayCase(PropertyInit.BASIC_ITEM));
 }
