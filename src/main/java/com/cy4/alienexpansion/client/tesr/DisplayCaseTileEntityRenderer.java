96ad0228657ba0f449970528c3a8fc58631789c5
Cy4Shot <veschi@gmail.com>
2020-04-02 14:56:41 +0100

    TESR Server Packet System

--- src/main/java/com/cy4/alienexpansion/client/tesr/DisplayCaseTileEntityRenderer.java
+++ src/main/java/com/cy4/alienexpansion/client/tesr/DisplayCaseTileEntityRenderer.java
@@ -14,27 +14,28 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 
 @SuppressWarnings("deprecation")
-public class DisplayCaseTileEntityRenderer extends TileEntityRenderer<DisplayCaseTileEntity>{
+public class DisplayCaseTileEntityRenderer extends TileEntityRenderer<DisplayCaseTileEntity> {
 
 	public DisplayCaseTileEntityRenderer(TileEntityRendererDispatcher rendererDispatcherIn) {
 		super(rendererDispatcherIn);
 	}
-	
+
 	@Override
 	public void render(DisplayCaseTileEntity tileEntityIn, float partialTicks, MatrixStack matrixStackIn,
 			IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn) {
-	         ItemStack itemstack = tileEntityIn.getInventory();
-	         AlienExpansionMod.LOGGER.debug(itemstack);
-	         if (itemstack != ItemStack.EMPTY) {
-	            matrixStackIn.push();
-	            matrixStackIn.translate(0.5D, 0.44921875D, 0.5D);
-	            float f = -Direction.byHorizontalIndex((0 + Direction.NORTH.getHorizontalIndex()) % 4).getHorizontalAngle();
-	            matrixStackIn.rotate(Vector3f.YP.rotationDegrees(f));
-	            matrixStackIn.rotate(Vector3f.XP.rotationDegrees(90.0F));
-	            matrixStackIn.translate(-0.3125D, -0.3125D, 0.0D);
-	            matrixStackIn.scale(0.375F, 0.375F, 0.375F);
-	            Minecraft.getInstance().getItemRenderer().renderItem(itemstack, ItemCameraTransforms.TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrixStackIn, bufferIn);
-	            matrixStackIn.pop();
-	         }
+		ItemStack itemstack = tileEntityIn.getInventory();
+		AlienExpansionMod.LOGGER.debug(itemstack);
+		if (itemstack != ItemStack.EMPTY) {
+			matrixStackIn.push();
+			matrixStackIn.translate(0.5D, 0.44921875D, 0.5D);
+			float f = -Direction.byHorizontalIndex((0 + Direction.NORTH.getHorizontalIndex()) % 4).getHorizontalAngle();
+			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(f));
+			matrixStackIn.rotate(Vector3f.XP.rotationDegrees(90.0F));
+			matrixStackIn.translate(-0.3125D, -0.3125D, 0.0D);
+			matrixStackIn.scale(0.375F, 0.375F, 0.375F);
+			Minecraft.getInstance().getItemRenderer().renderItem(itemstack, ItemCameraTransforms.TransformType.FIXED,
+					combinedLightIn, combinedOverlayIn, matrixStackIn, bufferIn);
+			matrixStackIn.pop();
+		}
 	}
 }
--- src/main/java/com/cy4/alienexpansion/common/tile/DisplayCaseTileEntity.java
+++ src/main/java/com/cy4/alienexpansion/common/tile/DisplayCaseTileEntity.java
@@ -1,11 +1,14 @@
 package com.cy4.alienexpansion.common.tile;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
+import com.cy4.alienexpansion.AlienExpansionMod;
 import com.cy4.alienexpansion.core.init.TileInit;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.play.server.SUpdateTileEntityPacket;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
@@ -15,46 +18,80 @@ import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
-public class DisplayCaseTileEntity extends TileEntity implements ITickableTileEntity{
+public class DisplayCaseTileEntity extends TileEntity implements ITickableTileEntity {
 	private ItemStackHandler handler;
-	
+
 	public DisplayCaseTileEntity(TileEntityType<?> tetype) {
 		super(tetype);
 	}
-	
+
 	public DisplayCaseTileEntity() {
 		this(TileInit.DISPLAY_CASE.get());
 	}
 
 	@Override
-	public void tick() { }
-	
+	public void tick() {
+		AlienExpansionMod.LOGGER.debug(getHandler().getStackInSlot(0));
+	}
+
 	@Override
 	public void read(CompoundNBT tag) {
 		getHandler().deserializeNBT(tag.getCompound("inv"));
 		super.read(tag);
 	}
-	
+
 	@Override
 	public CompoundNBT write(CompoundNBT tag) {
 		tag.put("inv", getHandler().serializeNBT());
 		return super.write(tag);
 	}
-	
+
 	private ItemStackHandler getHandler() {
-		if(handler == null) handler = new ItemStackHandler(1);
+		if (handler == null)
+			handler = new ItemStackHandler(1) {
+				@Nonnull
+				@Override
+				public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+					inventoryChanged();
+					return super.insertItem(slot, stack, simulate);
+				}
+
+				@Override
+				public ItemStack extractItem(int slot, int amount, boolean simulate) {
+					inventoryChanged();
+					return super.extractItem(slot, amount, simulate);
+				}
+			};
 		return handler;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Nonnull
 	@Override
 	public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
-		if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) return LazyOptional.of(() -> (T) getHandler());
+		if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+			return LazyOptional.of(() -> (T) getHandler());
 		return super.getCapability(cap, side);
 	}
 
 	public ItemStack getInventory() {
-		return getHandler().getStackInSlot(0);
+		inventoryChanged();
+		return handler.getStackInSlot(0);
+	}
+
+	private void inventoryChanged() {
+		this.markDirty();
+		this.getWorld().notifyBlockUpdate(this.getPos(), this.getBlockState(), this.getBlockState(), 3);
+	}
+
+	@Nullable
+	@Override
+	public SUpdateTileEntityPacket getUpdatePacket() {
+		return new SUpdateTileEntityPacket(this.pos, 13, this.getUpdateTag());
+	}
+
+	@Override
+	public CompoundNBT getUpdateTag() {
+		return this.write(new CompoundNBT());
 	}
 }
